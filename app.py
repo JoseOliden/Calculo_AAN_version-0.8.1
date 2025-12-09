88dabb00bf26abaa8c830222832e4488d4156e5c
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-09 12:09:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -315,11 +315,14 @@ elif page == "📊 Procesamiento":
             tr_c_Au = st.session_state.t_real_au
             geom = st.session_state.geometria
             
-            C, Q0_alfa_i = conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_c_Au,tr_c_Au, alfa, f, geom)
+            C, Q0_alfa_i,Q0_alfa_c_Au, Aesp_i, Aesp_c_Au = conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_c_Au,tr_c_Au, alfa, f, geom)
             st.write(C*1000000)
             df_muestra["Concentracion (PPM)"] = C*1000000
             st.dataframe(df_muestra)
             st.write(Q0_alfa_i)
+            st.write(Q0_alfa_c_Au)
+            st.write(Aesp_i)
+            st.write(Aesp_c_Au)
 # ============================================
 # SECCIÓN 4: RESULTADOS
 # ============================================

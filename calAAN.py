99570466fe0ae23c67d478e01ba085bbf0323086
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-09 11:50:58 -0500

    Update calAAN.py

--- calAAN.py
+++ calAAN.py
@@ -136,7 +136,7 @@ def conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_
   for i in range(len(k0_i)):
     # Calcula la concentración del elemento i en la muestra
     C[i] = (Aesp_i[i]/Aesp_c_Au)*(k0_c_Au/k0_i[i])*(e_c_Au/e_i[i])*((f + Q0_alfa_c_Au)/(f + Q0_alfa_i[i]))
-  
+    st.write( Q0_alfa_i[i])
   return C 
 
 # ------------------------ Calculo de Incertidumbre ---------------------------#
